b39c03f086c785c57ed66d8bf6cce3496b808e52
SSU <103107905+SeolSongWoo@users.noreply.github.com>
2024-05-23 18:15:38 +0900

    Initial commit

---
+++ src/main/java/com/poe/gamble/PoeGambleApplication.java
@@ -0,0 +1,13 @@
+package com.poe.gamble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoeGambleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoeGambleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poe/gamble/PoeGambleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poe.gamble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoeGambleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
