8372071d44336698c357a891812d5dc829eb42ff
SSU <103107905+SeolSongWoo@users.noreply.github.com>
2024-06-17 10:55:46 +0900

    FEAT 모든 Reqeust에 대한 Logging 추가

--- src/main/java/com/poe/gamble/config/WebConfig.java
+++ src/main/java/com/poe/gamble/config/WebConfig.java
@@ -1,17 +1,34 @@
 package com.poe.gamble.config;
 
 import com.poe.gamble.aop.CurrentUserEmailArgumentResolver;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new CurrentUserEmailArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                log.info("Request: URL: {}, HTTP-Method: {}", request.getRequestURL(), request.getMethod());
+                return true;
+            }
+        });
     }
 }
